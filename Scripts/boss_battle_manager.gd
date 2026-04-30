34c712b9068621f24086d4eb9b27aa02b50c0c71
Cole Hauser <158326781+Piggo15@users.noreply.github.com>
2026-04-30 10:07:26 -0400

    Flowe Work

--- Scripts/boss_battle_manager.gd
+++ Scripts/boss_battle_manager.gd
@@ -24,6 +24,8 @@ var bullet_scene = preload("res://Prefab Scenes/enemy_bulllet.tscn")
 @onready var shoot_point: Node3D = $Boss/Area3D/Shoot_Point
 @onready var shoot_sound: AudioStreamPlayer3D = $Boss/Shoot_Sound
 @onready var health_bar: ColorRect = $Control/Health_Bar
+@onready var start_timer: Timer = $Start_Timer
+@onready var flash_before_phase_2_timer: Timer = $FlashBeforePhase2Timer
 
 var current_stage = 1
 var current_audio_state = 0
@@ -35,6 +37,16 @@ var current_audio_state = 0
 @onready var pos_4: Node3D = $Pos4
 
 @onready var positions_to_teleport_to = [pos_1, pos_2, pos_3, pos_4]
+
+@onready var goon_pos_1: Node3D = $GoonPos1
+@onready var goon_pos_2: Node3D = $GoonPos2
+@onready var goon_pos_3: Node3D = $GoonPos3
+@onready var goon_pos_4: Node3D = $GoonPos4
+@onready var goon_pos_5: Node3D = $GoonPos5
+@onready var goon_pos_6: Node3D = $GoonPos6
+
+@onready var goon_positions = [goon_pos_1, goon_pos_2, goon_pos_3, goon_pos_4, goon_pos_5, goon_pos_6]
+
 var current_pos = pos_0
 
 var loop_1_played_times = 0
@@ -44,9 +56,14 @@ var max_health = 40
 
 var teleport_cooldown = 3
 var pre_teleport_time = 1
+var pre_teleport_time_phase_2 = 0.5
 var shot_cooldown = 0.5
+var teleport_cooldown_phase_2 = 2
+var shot_cooldown_phase_2 = 0.25
 var shots_fired = 0
 var shooter_force = 60
+var shots_per_teleport = 4
+var shots_per_teleport_phase_2 = 6
 
 @onready var flash_ps: GPUParticles3D = $Boss/Flash_PS
 
@@ -61,22 +78,37 @@ func music_switcher():
 	match current_audio_state:
 		
 		0:
-			if !music_player.playing:
-				music_player.stream = BOSS_BATTLE_PHASE_1_LOOP
-				music_player.play()
-				current_audio_state = 1
-				loop_1_played_times = 1
+			if music_player.playing:
+				return
+			music_player.stream = BOSS_BATTLE_PHASE_1_LOOP
+			music_player.play()
+			current_audio_state = 1
+			loop_1_played_times = 1
 		
 		1:
-			if !music_player.playing:
-				if loop_1_played_times > 2:
-					music_player.stream = BOSS_BATTLE_PHASE_1_NO_DRUM_VARIANT
-					music_player.play()
-					loop_1_played_times = 0
-				else:
-					music_player.stream = BOSS_BATTLE_PHASE_1_LOOP
-					music_player.play()
-					loop_1_played_times += 1
+			if music_player.playing:
+				return
+			if loop_1_played_times > 2:
+				music_player.stream = BOSS_BATTLE_PHASE_1_NO_DRUM_VARIANT
+				music_player.play()
+				loop_1_played_times = 0
+			else:
+				music_player.stream = BOSS_BATTLE_PHASE_1_LOOP
+				music_player.play()
+				loop_1_played_times += 1
+		2:
+			if music_player.playing:
+				return
+			music_player.stream = BOSS_BATTLE_PHASE_2_INTRO
+			music_player.play()
+			current_audio_state = 3
+		
+		3:
+			if music_player.playing:
+				return
+			music_player.stream = BOSS_BATTLE_PHASE_2_LOOP
+			music_player.play()
+			
 
 func _on_music_finished():
 	if player.died == true:
@@ -85,6 +117,8 @@ func _on_music_finished():
 
 func _process(_delta: float) -> void:
 	boss_body.look_at (player.global_position, Vector3.UP)
+	if player.died == true:
+		music_player.stop()
 
 func update_health_bar():
 	health_bar.scale.x = float(health) / float(max_health)
@@ -135,7 +169,7 @@ func _on_shoot_timer_timeout() -> void:
 	shots_fired += 1
 	shoot_sound.pitch_scale = randf_range(0.9, 1.2)
 	shoot_sound.play()
-	if shots_fired >= 4:
+	if shots_fired >= shots_per_teleport:
 		shots_fired = 0
 	else:
 		shoot_timer.start(shot_cooldown)
@@ -145,3 +179,20 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 	body.queue_free()
 	health -= 1
 	update_health_bar()
+	if (float(health) / float(max_health)) <= 0.5 and current_stage == 1:
+		current_stage = 2
+		current_audio_state = 2
+		teleport_timer.stop()
+		flash_ps.emitting = true
+		tp_sound.play()
+		start_timer.start(music_player.stream.get_length() - music_player.get_playback_position())
+		teleport_cooldown = teleport_cooldown_phase_2
+		shot_cooldown = shot_cooldown_phase_2
+		shots_per_teleport = shots_per_teleport_phase_2
+		pre_teleport_time = pre_teleport_time_phase_2
+		shots_fired = shots_per_teleport
+		flash_before_phase_2_timer.start(0.1)
+
+
+func _on_flash_before_phase_2_timer_timeout() -> void:
+	boss.position = pos_0.global_position
