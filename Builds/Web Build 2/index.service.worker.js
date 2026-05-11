6a8f3b45037af7455541ace1ec23690ea5a748d9
Cole Hauser <158326781+Piggo15@users.noreply.github.com>
2026-05-11 10:22:03 -0400

    Build

--- Builds/Web Build 2/index.service.worker.js
+++ Builds/Web Build 2/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778507715|1046184968';
+const CACHE_VERSION = '1778509312|278460729';
 /** @type {string} */
 const CACHE_PREFIX = 'Ten Game-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
